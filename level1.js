892526e2f46cb14b799d436d9e6f8b7e5b328dbf
fatema <fatema1992xx@gmail.com>
2024-05-08 20:26:35 +0300

    add function to updateLifeUI

--- level1.js
+++ level1.js
@@ -1,158 +1,155 @@
-const cards = document.querySelectorAll('.card');
-let matched = 0;
-let cardOne, cardTwo;
-let disableDeck = false;
-let openCards = [];
-let countdownElement = document.getElementById('second');
-let lifeCount = 3;
-let interval; // Declare interval outside the countdown function
+const cards = document.querySelectorAll('.card')
+let matched = 0
+let cardOne, cardTwo
+let disableDeck = false
+let openCards = []
+let countdownElement = document.getElementById('second')
+let lifeCount = 3
+let interval
 let cardsList = [
 { img: 'img-1.jpg', flips: 0 },
 { img: 'img-2.jpg', flips: 0 },
 { img: 'img-3.jpg', flips: 0 },
 { img: 'img-4.jpg', flips: 0 }
-];
-function decrementLife() {
- lifeCount--; // Decrement the life count
-console.log('life decremented');
- updateLifeUI(); // Update the UI to reflect the new life count
+]
+const decrementLife=(() =>{
+lifeCount-- 
+console.log('life decremented')
+ updateLifeUI() // Update the UI to reflect the new life count
 
 if (lifeCount === 0) {
-   // Game over logic here
-   //alert('Game Over! You have run out of lives.');
-clearInterval(interval);
-countdownElement.innerHTML = 'Gave Over! No lifes left.. Try again';
-   // Add any additional logic here, such as resetting the game or showing a replay button
+clearInterval(interval)
+countdownElement.innerHTML = 'Gave Over! No lifes left.. Try again'
 }
-flips = 0;
-}
-function updateLifeUI() {
-const lifeImages = document.querySelectorAll('.lifes img');
+flips = 0
+})
+const updateLifeUI=(() =>{
+const lifeImages = document.querySelectorAll('.lifes img')
 for (let i = 0; i < lifeImages.length; i++) {
 if (i < lifeCount) {
-    lifeImages[i].style.display = 'block';
+    lifeImages[i].style.display = 'block'
 } else {
-    lifeImages[i].style.display = 'none';
+    lifeImages[i].style.display = 'none'
 }
 }
-}
+})
 let updateFlips = (img) => {
  // Find the index of the image in cardsList
 let imageName =
-img.substring(img.lastIndexOf('/') + 1, img.lastIndexOf('.')) + '.jpg';
-console.log(imageName);
-let index = cardsList.findIndex((card) => card.img === imageName);
+img.substring(img.lastIndexOf('/') + 1, img.lastIndexOf('.')) + '.jpg'
+console.log(imageName)
+let index = cardsList.findIndex((card) => card.img === imageName)
 
 if (index !== -1) {
    // Increment the flips count for the image
-cardsList[index].flips++;
+cardsList[index].flips++
 
 console.log(
     'Current flip count for ' + img + ' is: ' + cardsList[index].flips
 );
 if (cardsList[index].flips > 2) {
-    decrementLife();
-     cardsList[index].flips = 0; //reset flips for current image
+    decrementLife()
+     cardsList[index].flips = 0 //reset flips for current image
 }
 } else {
-console.log('Image ' + img + ' not found in cardsList');
+console.log('Image ' + img + ' not found in cardsList')
 }
-};
+}
 let flipCard = ({ target: clickedCard }) => {
 if (cardOne !== clickedCard && !disableDeck) {
-clickedCard.classList.add('flip');
+clickedCard.classList.add('flip')
 if (!cardOne) {
-    return (cardOne = clickedCard);
+    return (cardOne = clickedCard)
 }
-cardTwo = clickedCard;
-disableDeck = true;
+cardTwo = clickedCard
+disableDeck = true
 let cardOneImg = cardOne.querySelector('.back-view img').src,
-    cardTwoImg = cardTwo.querySelector('.back-view img').src;
-matchCards(cardOneImg, cardTwoImg);
+    cardTwoImg = cardTwo.querySelector('.back-view img').src
+matchCards(cardOneImg, cardTwoImg)
 }
-};
+}
 let matchCards = (img1, img2) => {
 
 if (img1 === img2) {
-matched++;
+matched++
 const pushToOpenCards = (matchCards) => {
-    openCards.push(matchCards);
+    openCards.push(matchCards)
     if ((openCards.length = 3)) {
     }
-};
-pushToOpenCards(matchCards);
-console.log(openCards);
+}
+pushToOpenCards(matchCards)
+console.log(openCards)
 if (matched == 4) {
     setTimeout(() => {
-    return shuffleCard();
-    }, 1000);
+    return shuffleCard()
+    }, 1000)
 }
-cardOne.removeEventListener('click', flipCard);
-cardTwo.removeEventListener('click', flipCard);
-cardOne = cardTwo = '';
-return (disableDeck = false);
+cardOne.removeEventListener('click', flipCard)
+cardTwo.removeEventListener('click', flipCard)
+cardOne = cardTwo = ''
+return (disableDeck = false)
 } else {
-updateFlips(img1);
-shakeAndReset();
+updateFlips(img1)
+shakeAndReset()
 }
-};
-function shakeAndReset() {
+}
+const shakeAndReset=(()=> {
 setTimeout(() => {
-cardOne.classList.add('shake');
-cardTwo.classList.add('shake');
-}, 400);
+cardOne.classList.add('shake')
+cardTwo.classList.add('shake')
+}, 400)
 
 setTimeout(() => {
-cardOne.classList.remove('shake', 'flip');
-cardTwo.classList.remove('shake', 'flip');
-cardOne = cardTwo = '';
-disableDeck = false;
-}, 1200);
-}
+cardOne.classList.remove('shake', 'flip')
+cardTwo.classList.remove('shake', 'flip')
+cardOne = cardTwo = ''
+disableDeck = false
+}, 1200)
+})
 
 let shuffleCard = () => {
-matched = 0;
-disableDeck = false;
-cardOne = cardTwo = '';
-let arr = [1, 2, 3, 4, 1, 2, 3, 4];
-arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+matched = 0
+disableDeck = false
+cardOne = cardTwo = ''
+let arr = [1, 2, 3, 4, 1, 2, 3, 4]
+arr.sort(() => (Math.random() > 0.5 ? 1 : -1))
 cards.forEach((card, i) => {
-card.classList.remove('flip');
-let imgTag = card.querySelector('.back-view img');
-imgTag.src = `cards/img-${arr[i]}.JPG`;
-card.addEventListener('click', flipCard);
-});
-};
-shuffleCard();
+card.classList.remove('flip')
+let imgTag = card.querySelector('.back-view img')
+imgTag.src = `cards/img-${arr[i]}.JPG`
+card.addEventListener('click', flipCard)
+})
+}
+shuffleCard()
 
 cards.forEach((card) => {
-card.addEventListener('click', flipCard);
+card.addEventListener('click', flipCard)
 });
 
 let countdown = (minutes) => {
- let seconds = minutes * 60;
-interval = setInterval(function () {
-let minutesRemaining = Math.floor(seconds / 60);
-let secondsRemaining = seconds % 60;
+ let seconds = minutes * 60
+interval = setInterval( () =>{
+let minutesRemaining = Math.floor(seconds / 60)
+let secondsRemaining = seconds % 60
 
 countdownElement.innerHTML =
     minutesRemaining +
     ':' +
     (secondsRemaining < 10 ? '0' : '') +
-    secondsRemaining;
+    secondsRemaining
 
 if (--seconds < 0) {
-    clearInterval(interval);
-    countdownElement.innerHTML = 'Time Up .. Try again';
-    shuffleCard();
-    countdown(1);
+    clearInterval(interval)
+    countdownElement.innerHTML = 'Time Up .. Try again'
+    shuffleCard()
+    countdown(1)
 }
 if (matched == 4) {
-    clearInterval(interval);
-    countdownElement.innerHTML = 'You Win !!!';
-    countdown(1);
+    clearInterval(interval)
+    countdownElement.innerHTML = 'You Win !!!'
+    countdown(1)
 }
-}, 1000);
-};
+}, 1000)
+}
 
-countdown(1);
+countdown(1)
